b483b2e37d707fd2af493e96a5b50fec8adc7ada
Your Name <plumge@gmail.com>
2019-11-13 15:32:50 -0500

    added images to cards

--- src/components/CreateArtistCard.js
+++ src/components/CreateArtistCard.js
@@ -2,7 +2,21 @@ const Http = require("../utils/http");
 const Deact = require("../libs/deact");
 
 function CreateArtistCard(content) {
-  return Deact.create("section", { class: "artist-card " }, content.name);
+  return Deact.create("section", { class: "artist-card " }, [
+    Deact.create("h2", { class: `artist-card__name` }, content.name),
+    Deact.create("img", { class: `artist-card__image`, src: content.image }, "")
+  ]);
 }
 
 module.exports = CreateArtistCard;
+
+// return Deact.create("section", { class: `student-card` }, [
+//   Deact.create("h3", { class: `student-card__name` }, student.name),
+//   Deact.create(
+//     "img",
+//     { class: `student-card__avatar`, src: student.imageUrl },
+//     ""
+//   ),
+//   Deact.create("p", { class: `student-card__language` }, student.language)
+// ]);
+// }
